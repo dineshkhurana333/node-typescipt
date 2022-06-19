59117ca2e4ef1e45789aeccfc8fafa959fb309cb
dinesh k <dineshkhurana333@gmail.com>
2022-06-19 12:21:17 +0530

    setup mongo cloud database

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/nodetype', {
+mongoose.connect('mongodb+srv://dinesh:dinesh123@cluster0.sjrp1dp.mongodb.net', {
+  dbName: 'NodeType',
   connectTimeoutMS: 100,
 }).catch(err => {
   console.log('Error in db connectivity:: ', err);
   process.exit(1)
-}).then(()=>{
+}).then(() => {
   console.log('db connected!!')
 });
 
